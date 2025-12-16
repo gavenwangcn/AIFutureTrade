31615564dc4a94a0ff6b262865f3e41a9d277395
61444803@qq.com <61444803@qq.com>
2025-12-16 20:33:24 +0800

    update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -12,7 +12,7 @@ backlog = int(os.getenv('GUNICORN_BACKLOG', '2048'))
 workers = int(os.getenv('GUNICORN_WORKERS', multiprocessing.cpu_count() * 2 + 1))
 worker_class = os.getenv('GUNICORN_WORKER_CLASS', 'eventlet')
 worker_connections = int(os.getenv('GUNICORN_WORKER_CONNECTIONS', '1000'))
-threads = int(os.getenv('GUNICORN_THREADS', '40'))
+threads = int(os.getenv('GUNICORN_THREADS', '60'))
 timeout = int(os.getenv('GUNICORN_TIMEOUT', '120'))
 keepalive = int(os.getenv('GUNICORN_KEEPALIVE', '30'))
 
