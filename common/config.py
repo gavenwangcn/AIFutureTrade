b0b323f0fdd24905f45daf3f344cf3e5013bde9f
61444803@qq.com <61444803@qq.com>
2025-12-16 22:02:03 +0800

    update

--- common/config.py
+++ common/config.py
@@ -134,13 +134,13 @@ TRADES_QUERY_LIMIT = int(os.getenv('TRADES_QUERY_LIMIT', '5'))  # 后端查询�
 
 # ============ AI交易决策配置 ============
 
-PROMPT_MARKET_SYMBOL_LIMIT = 5  # 每次调用AI模型时处理的合约数量
+PROMPT_MARKET_SYMBOL_LIMIT = 3  # 每次调用AI模型时处理的合约数量
 BUY_DECISION_THREAD_COUNT = 1  # 买入决策API调用的并发线程数
 SELL_DECISION_THREAD_COUNT = 1  # 卖出决策API调用的并发线程数
 AI_DECISION_SYMBOL_BATCH_SIZE = int(os.getenv('AI_DECISION_SYMBOL_BATCH_SIZE', '1'))  # 每次提交给AI模型的symbol数量，默认为1
 
 # AI批次执行间隔配置
-AI_BATCH_EXECUTION_INTERVAL = int(os.getenv('AI_BATCH_EXECUTION_INTERVAL', '30'))  # 批次执行间隔（秒），默认5秒
+AI_BATCH_EXECUTION_INTERVAL = int(os.getenv('AI_BATCH_EXECUTION_INTERVAL', '60'))  # 批次执行间隔（秒），默认5秒
 AI_BATCH_EXECUTION_GROUP_SIZE = int(os.getenv('AI_BATCH_EXECUTION_GROUP_SIZE', '1'))  # 每N个批次执行完成后统一处理（插入数据库和调用SDK），默认1
 
 
