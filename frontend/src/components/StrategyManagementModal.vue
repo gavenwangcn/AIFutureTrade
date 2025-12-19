e0378fc2bc7c84df7fa64dc2851ccf738e1e92df
00353913 <wangyipeng@mychery.com>
2025-12-19 15:02:25 +0800

    update

--- frontend/src/components/StrategyManagementModal.vue
+++ frontend/src/components/StrategyManagementModal.vue
@@ -181,7 +181,7 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import Modal from './Modal.vue'
 import { strategyApi } from '../services/api.js'
 
